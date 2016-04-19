dbb668714eb159ebd8c556b43a20b657a959c26b
Grigorenko <ingenichsk@mail.ru>
2016-04-19 23:11:25 +0300

    add new method factorial

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -20,11 +20,25 @@ namespace HelloGit
             return pow;
         }
 
+        static int Fact(int n)
+        {
+            int res = 1;
+
+            for (int i = 1; i < n+1; i++)
+            {
+                res *= i;
+            }
+
+            return res;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
 
-            Console.WriteLine(Pow(2,3));
+            Console.WriteLine("2 в степени 3 = "+Pow(2,3));
+
+            Console.WriteLine("Факториал 5 = " + Fact(5));
 
             Console.ReadKey();
         }
